799ee23fd2bcb9dbcbad1b1343a3028e896f64ae
oanagrecu <oanagrecu09@yahoo.com>
2024-03-14 20:48:39 +0100

    fix header padding and replace charts

--- content-footer.php
+++ content-footer.php
@@ -1,9 +1,7 @@
-<footer id="footer_content" class="site-footer  text-dark py-4" role="contentinfo">
-    <div class="container-fluid">
+<footer id="footer_content" class="container-fluid py-2 px-2 mt-4 text-dark text-nowrap" role="contentinfo">
         <div class="row">
-            <div class="col-md-12 text-center">
-                <p class="mb-0">&copy;<?php echo date("Y"); ?> Akwanza, All Rights Reserved</p>
+            <div class="col-md-12 col-sm-12 text-center">
+                <p class="text-center mb-0">&copy;<?php echo date("Y"); ?> Akwanza, All Rights Reserved</p>
             </div>
         </div>
-    </div>
 </footer>
--- content-section1.php
+++ content-section1.php
@@ -1,12 +1,13 @@
-<div class="container-fluid mt-5 mb-5">
-                <div class="row d-flex flex-row flex-wrap-reverse gx-5">
+<div class="container mt-4 mb-4">
+                <div class="row d-flex flex-row flex-wrap-reverse text-center gx-5">
                     <div class="col-sm-6">
-                        <h2 class="text-lg-end text-sm-end text-center text-nowrap">Wildlife Conservation</h2>
-                        <p class="text-lg-end text-sm-end text-center">
-                            The Big Life Foundation is currently patrolling approximately 1.6 million acres across the Amboseli-Tsavo-Kilimanjaro (Greater Amboseli) ecosystem in East Africa.
-                        </p>
+                        <h2 class="text-md-end text-sm-end text-center text-nowrap">Wildlife Conservation</h2>
+                        
+                        <p class="text-md-end text-sm-end  text-center ">The Big Life Foundation is currently patrolling approximately 1.6 million acres across the Amboseli-Tsavo-Kilimanjaro (Greater Amboseli) ecosystem in East Africa.
+                        </p> 
+                    
                     </div>
-                    <!-- <div id="chart" class="col-sm svg-container text-center"></div> -->
+                    <!-- <div id="chart" class="col-sm-6 svg-container text-center"></div> -->
                     <div class="flourish-embed flourish-map col-sm-6 text-center" data-src="visualisation/17095445"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
                 </div>
 </div>
