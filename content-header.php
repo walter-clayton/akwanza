3fb3ee72e7498391c74e9b21adfd77ee64d43de9
oanagrecu <oanagrecu09@yahoo.com>
2024-03-18 15:37:50 +0100

    trying some changes

--- content-header.php
+++ content-header.php
@@ -1,19 +1,37 @@
-<header class="container-fluid" id="container-header">
+<!-- <header class="container-fluid mx-0 px-0" id="container-header">
     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/akwanza_header_image.png" class="img-fluid cover-image" alt="Wildlife Image">
-    <div class="header-content">
-        <div class="row text-center ">
-            <p class=" col-lg text-white h6 header-tag-names">
+    <div class="container header-content">
+        <div class="row text-center">
+            <p class="col-lg text-white h6 header-tag-names">
                 STAFF 
                 <span class="col-lg h2 header-span">572</span>
             </p>
-            <p class="col-lg text-white h6 ">
+            <p class="col-lg text-white h6 header-tag-names">
                 WILD ANIMALS SAVED 
                 <span class="col-lg h2 text-white header-span">5</span>
             </p>
-            <p class="col-lg  text-white h6 ">
+            <p class="col-lg  text-white h6 header-tag-names">
                 ACRES RESTORED 
                 <span class="col-lg h2 text-white header-span">337</span> 
             </p>
         </div>
     </div>
+</header> -->
+<header class="header-top mx-auto d-block;" id="container-header" style="background-image: url(<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/akwanza_header_image.png); color: white;">
+   <div class="container text-center tex">
+       <div class="row">
+           <div class="col-md-4 div1"> <p class="col-md text-white h6 header-tag-names">
+                STAFF 
+                <span class="col-lg h2 header-span">572</span>
+            </p>  </div>
+           <div class="col-md-4 div2"><p class=" col-md text-white h6 header-tag-names">
+                WILD ANIMALS SAVED 
+                <span class="col-lg h2 text-white header-span">5</span>
+            </p></div>
+           <div class="col-md-4 div3"><p class="col-md text-white h6 header-tag-names">
+                ACRES RESTORED 
+                <span class="col-lg h2 text-white header-span">337</span> 
+            </p> </div>
+       </div>
+   </div>
 </header>
--- header.php
+++ header.php
@@ -4,14 +4,16 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Akwanza</title>
-    <!-- Include additional stylesheets, scripts, and metadata here -->
 </head>
 <body <?php body_class(); ?>>
 <header>
-    <div class="container-fluid mx-0 mr-0 px-0 pr-0">
-        <nav class="navbar navbar-expand-lg navbar">
-            <div class="container-fluid">
+    <div class="container-fluid mx-0 px-0">
+        <nav class="navbar navbar-expand-sm navbar">
+            <div class="container-fluid mx-0 px-0">
                 <a class="navbar-brand" href="<?php echo esc_url( home_url( '/home' ) ); ?>">
                     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/akwanza_logo.png" alt="akwanza Logo" width="60" height="60" style="color: black;">
                 </a>
@@ -36,4 +38,3 @@
         </nav>
     </div>
 </header>
-
