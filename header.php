9f66d261fd175e78413d676ecc7be0004979a515
oanagrecu <oanagrecu09@yahoo.com>
2024-03-18 20:13:34 +0100

    resolved issue with the collpase menu

--- header.php
+++ header.php
@@ -5,15 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
     <title>Akwanza</title>
-    <!-- Bootstrap JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script> 
     <!-- Include additional stylesheets, scripts, and metadata here -->
 </head>
 <body <?php body_class(); ?>>
 <header>
     <div class="container-fluid mx-0 px-0">
-        <nav class="navbar navbar-expand-lg navbar">
+        <nav class="navbar navbar-expand-md navbar">
             <div class="container-fluid mx-0 px-0">
                 <a class="navbar-brand" href="<?php echo esc_url( home_url( '/Home' ) ); ?>">
                     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/akwanza_logo.png" alt="akwanza Logo" width="60" height="60" style="color: black;">
@@ -23,21 +21,21 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link <?php echo is_front_page() ? 'active' : ''; ?>" href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link <?php echo is_page('About') ? 'active' : ''; ?>" href="<?php echo esc_url(get_permalink(get_page_by_path('about'))); ?>">About</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link <?php echo is_page('Reports') ? 'active' : ''; ?>" href="<?php echo esc_url(get_permalink(get_page_by_path('reports'))); ?>">Reports</a>
-        </li>    
-    </ul>
-</div>
-
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link <?php echo is_front_page() ? 'active' : ''; ?>" href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link <?php echo is_page('About') ? 'active' : ''; ?>" href="<?php echo esc_url(get_permalink(get_page_by_path('about'))); ?>">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link <?php echo is_page('Reports') ? 'active' : ''; ?>" href="<?php echo esc_url(get_permalink(get_page_by_path('reports'))); ?>">Reports</a>
+                        </li>    
+                    </ul>
+                </div>
             </div>
         </nav>
     </div>
 </header>
-
+</body>
+</html>
