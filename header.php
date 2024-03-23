a84b625d841ab95ddd89f9861d977c9c7fe99f5a
oanagrecu <oanagrecu09@yahoo.com>
2024-03-23 23:25:34 +0100

    adding t-shirt icon

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1.0 ">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
     <?php wp_head(); ?>
     <title>Akwanza</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -35,7 +37,9 @@
                     </li>
                 </ul>
                 <div id="button-navbar">
-                    <a href="https://akwanza.teemill.com" class=" btn btn-success my-2">Shop</a>
+                    <a href="https://akwanza.teemill.com" class="btn bt-lg btn-success"> <span
+                            style="margin-right:4px;"><i class="fas fa-tshirt"></i></span>
+                        Shop</a>
                 </div>
             </div>
         </div>
