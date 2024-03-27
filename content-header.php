8c6188efe668f86375ebef8f52365022a7b75797
Walter Clayton <walter.h.clayton@gmail.com>
2024-03-27 12:53:28 +0100

    AK-78 improving fixes, need to work on background image

--- content-header.php
+++ content-header.php
@@ -1,10 +1,11 @@
-<div class="container" id="header-container">
-    <div class="background">
-        <img class="crane" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/Header.svg"
-            class="img-fluid">
-        <div class="overlay">
-            <h1 style="font-family: 'Colonna MT', serif;">Uniting for Wildlife Conservation</h1>
-            <h4>Together, Every Purchase is a Pledge to Protect</h4>
-        </div>
+<!-- <div>
+    <img class="crane" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/Header.svg" class="img-fluid">
+    <div class="overlay">
+        <h1 style="font-family: 'Colonna MT', serif;">Uniting for Wildlife Conservation</h1>
+        <h4>Together, Every Purchase is a Pledge to Protect</h4>
     </div>
-</div>
+</div> -->
+<!-- <div class="bg-image text-center shadow-1-strong text-white" style="background-image: url('<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/Header.svg');">
+    <h1 style="font-family: 'Colonna MT', serif;">Uniting for Wildlife Conservation</h1>
+    <h4>Together, Every Purchase is a Pledge to Protect</h4>
+</div> -->
--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <div id="primary" class="content-area">
-    <main id="main" class="site-main row" role="main">
+    <main id="main" class="site-main" role="main">
         <?php get_header(); ?>
-        <?php include 'content-header.php'; ?>
+        <!-- <?php include 'content-header.php'; ?> -->
         <?php include 'content-section1.php'; ?>
 
         <?php
-        if (have_posts()):
-            while (have_posts()):
+        if (have_posts()) :
+            while (have_posts()) :
                 the_post();
-                ?>
+        ?>
                 <article <?php post_class(""); ?>>
 
                 </article><!-- #post-## -->
-                <?php
+        <?php
                 // If comments are open or we have at least one comment, load up the comment template.
-                if (comments_open() || get_comments_number()):
+                if (comments_open() || get_comments_number()) :
                     comments_template();
                 endif;
             endwhile;
             the_posts_navigation();
-        else:
+        else :
             get_template_part('content-none');
         endif;
         ?>
 
     </main><!-- #main -->
 </div><!-- #primary -->
-<?php include 'content-footer.php'; ?>
+<!-- <?php include 'content-footer.php'; ?> -->
